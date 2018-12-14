f073b7bf15afb318d7ff3cdb399f431b88d64ec0
Artsiom <artkostm@gmail.com>
2018-12-14 13:23:35 +0300

    add tests

--- src/test/scala/by/artsiom/bigdata201/yarn/Main.scala
+++ src/test/scala/by/artsiom/bigdata201/yarn/Main.scala
@@ -1,3 +1,24 @@
 package by.artsiom.bigdata201.yarn
 
-object Main {}
+import com.github.sakserv.minicluster.impl.{HdfsLocalCluster, YarnLocalCluster}
+import org.apache.hadoop.conf.Configuration
+import org.scalatest.FlatSpec
+
+class TestHdfsUtility extends FlatSpec  {
+
+  val hdfsLocalCluster = new HdfsLocalCluster.Builder()
+    .setHdfsNamenodePort(12345)
+    .setHdfsNamenodeHttpPort(12341)
+    .setHdfsTempDir("embedded_hdfs")
+    .setHdfsNumDatanodes(1)
+    .setHdfsEnablePermissions(false)
+    .setHdfsFormat(true)
+    .setHdfsEnableRunningUserAsProxyUser(true)
+    .setHdfsConfig(new Configuration())
+    .build()
+
+  "This test" should "start/stop mincluster" in {
+    hdfsLocalCluster.start()
+    hdfsLocalCluster.stop()
+  }
+}
