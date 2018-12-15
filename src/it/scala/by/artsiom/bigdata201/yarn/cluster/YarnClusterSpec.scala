1a8140b4ebab7d2a9c97c1764d317bc8bec00842
Artsiom <artkostm@gmail.com>
2018-12-15 22:01:04 +0300

    update yarn cluster

--- src/it/scala/by/artsiom/bigdata201/yarn/cluster/YarnClusterSpec.scala
+++ src/it/scala/by/artsiom/bigdata201/yarn/cluster/YarnClusterSpec.scala
@@ -1,32 +1,31 @@
 package by.artsiom.bigdata201.yarn.cluster
 
-import com.github.sakserv.minicluster.MiniCluster
 import com.github.sakserv.minicluster.impl.YarnLocalCluster
 import org.apache.hadoop.conf.Configuration
 import org.scalatest.{BeforeAndAfterAll, Suite}
 
-trait YarnCluster$ extends BeforeAndAfterAll { this: Suite =>
+trait YarnClusterSpec extends BeforeAndAfterAll { this: Suite =>
   override protected def beforeAll(): Unit = {
     super.beforeAll()
-    YarnCluster$.start()
+    YarnClusterSpec.start()
   }
 
   override protected def afterAll(): Unit = {
     super.afterAll()
-    YarnCluster$.stop()
+    YarnClusterSpec.stop()
   }
 }
 
-object YarnCluster$ extends Cluster {
-  override protected val cluster: MiniCluster = new YarnLocalCluster.Builder()
+object YarnClusterSpec extends Cluster {
+  override protected val cluster = new YarnLocalCluster.Builder()
     .setNumNodeManagers(1)
     .setNumLocalDirs(1)
     .setNumLogDirs(1)
-    .setResourceManagerAddress("localhost:37001")
-    .setResourceManagerHostname("localhost")
-    .setResourceManagerSchedulerAddress("localhost:37002")
-    .setResourceManagerResourceTrackerAddress("localhost:37003")
-    .setResourceManagerWebappAddress("localhost:37004")
+    .setResourceManagerAddress("0.0.0.0:8032")
+    .setResourceManagerHostname("0.0.0.0")
+    .setResourceManagerSchedulerAddress("0.0.0.0:37002")
+    .setResourceManagerResourceTrackerAddress("0.0.0.0:37003")
+    .setResourceManagerWebappAddress("0.0.0.0:37004")
     .setUseInJvmContainerExecutor(false)
     .setConfig(new Configuration())
     .build()
