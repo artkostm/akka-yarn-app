4ba5df5c64dff65fd82e1e88f52359959206a542
Artsiom <artkostm@gmail.com>
2018-12-15 22:00:25 +0300

    update test config

--- src/it/scala/by/artsiom/bigdata201/yarn/Main.scala
+++ src/it/scala/by/artsiom/bigdata201/yarn/Main.scala
@@ -1,12 +1,30 @@
 package by.artsiom.bigdata201.yarn
 
-import by.artsiom.bigdata201.yarn.cluster.{HdfsClusterSpec, YarnClusterSpec}
+import akkeeper.launcher.LauncherMain
+import by.artsiom.bigdata201.yarn.cluster.{HdfsClusterSpec, YarnClusterSpec, ZookeeperClusterSpec}
+import org.apache.hadoop.fs.Path
 import org.scalatest.FlatSpec
 
-class TestHdfsUtility extends FlatSpec with HdfsClusterSpec with YarnClusterSpec {
+class TestHdfsUtility extends FlatSpec with HdfsClusterSpec with YarnClusterSpec with ZookeeperClusterSpec {
   "This test" should "start/stop mincluster" in {
     println("Running!!!!")
-    Thread.sleep(5000)
-    Info.name
+    fs.listStatus(new Path("/")).foreach { fileStatus =>
+      println(fileStatus.getPath)
+    }
+    println("Summary")
+    println(fs.getContentSummary(new Path("/")))
+    fs.copyFromLocalFile(new Path("src/it/resources/test.conf"), new Path("/test.conf"))
+
+    println(fs.getContentSummary(new Path("/test.conf")))
+    println(s"traget/scala-2.12/${BuildInfo.JarName}")
+
+    LauncherMain.main(Array(
+      "--akkeeperJar", "src/it/resources/akkeeper-assembly-0.3.3.jar",
+      "--config", "src/it/resources/test.conf",
+      "--jars", "src/it/resources/hadoop-common-2.7.3.2.6.3.0-235.jar,src/it/resources/hadoop-hdfs-2.7.3.2.6.3.0-235.jar",
+      s"target/scala-2.12/${BuildInfo.JarName}"
+    ))
+    Thread.sleep(60000 * 4)
+    // --config ./config.conf /path/to/my.jar
   }
 }
