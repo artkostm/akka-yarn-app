9384de5825e196a14cfd7bf69052bc9c30672ab4
Artsiom Chuiko <artkostm@gmail.com>
2018-12-17 15:45:07 +0300

    akkeeper master test actor

--- src/multi-jvm/scala/by/artsiom/bigdata201/yarn/AkkeeperMasterTestActor.scala
+++ src/multi-jvm/scala/by/artsiom/bigdata201/yarn/AkkeeperMasterTestActor.scala
@@ -1,3 +1,12 @@
-package by.artsiom.bigdata201.yarn class AkkeeperMasterTestActor {
+package by.artsiom.bigdata201.yarn
+import akka.actor.Actor
+import akkeeper.api.{DeployContainer, OperationFailed, SubmittedInstances}
 
+class AkkeeperMasterTestActor extends Actor {
+  override def receive: Receive = {
+    case DeployContainer(name, _, _, _, reqId) if name == "wrongName" =>
+      sender ! OperationFailed(reqId, new RuntimeException("no containers with name " + name))
+    case DeployContainer(name, _, _, _, reqId) =>
+      sender ! SubmittedInstances(reqId, name, Seq.empty)
+  }
 }
