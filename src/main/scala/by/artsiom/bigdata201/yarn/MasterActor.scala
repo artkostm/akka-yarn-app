9cb89309165c71e7f959a306eb2e64ff8a21f3fa
Artsiom Chuiko <artkostm@gmail.com>
2018-12-17 15:43:46 +0300

    update master actor

--- src/main/scala/by/artsiom/bigdata201/yarn/MasterActor.scala
+++ src/main/scala/by/artsiom/bigdata201/yarn/MasterActor.scala
@@ -1,25 +1,34 @@
 package by.artsiom.bigdata201.yarn
 
-import akka.actor.{Actor, Kill, RootActorPath}
+import akka.actor.Status.Failure
+import akka.actor.{Actor, ActorLogging, RootActorPath}
 import akka.actor.SupervisorStrategy.Stop
 import akka.cluster.Cluster
 import akka.pattern.ask
-import akka.util.Timeout
+import akka.stream.ActorMaterializer
+import akka.stream.alpakka.hdfs._
+import akka.stream.alpakka.hdfs.scaladsl.HdfsFlow
+import akka.stream.scaladsl.{Sink, Source}
+import akka.util.{ByteString, Timeout}
 import akkeeper.api._
 import akkeeper.master.service.DeployService
 import by.artsiom.bigdata201.yarn.Messages.{RunTasks, Task, TaskResult}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
 
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
+import scala.util.Success
+import Constants._
 
 /**
- * Master actor that deploys workers to yarn containers
+ * Master actor that deploys workers to yarn containers and writes result to HDFS
  */
-class MasterActor extends Actor with MasterBehaviour {
+class MasterActor extends Actor with MasterBehaviour with ActorLogging {
   implicit val timeout  = Timeout(5 seconds)
   implicit val executor = context.system.dispatcher
 
-  val filePath = context.system.settings.config.getString("hdfs.file")
+  val filePath = context.system.settings.config.getString(HdfsFileSettingName)
+  val outDir = context.system.settings.config.getString(HdfsOutputDirectorySettingName)
 
   val deployService = DeployService.createRemote(context.system)
 
@@ -28,9 +37,9 @@ class MasterActor extends Actor with MasterBehaviour {
   override def preStart(): Unit = cluster.subscribe(self)
 
   cluster.registerOnMemberUp {
-    (deployService ? DeployContainer("worker", 5)).onComplete {
+    (deployService ? DeployContainer(WorkerActorRole, 5)).onComplete {
       case Success(SubmittedInstances(_, _, _)) => self ! RunTasks
-      case Success(_) | Failure(_) =>
+      case Success(_) | scala.util.Failure(_) =>
         context.become(failed)
     }
   }
@@ -41,21 +50,40 @@ class MasterActor extends Actor with MasterBehaviour {
 }
 
 trait MasterBehaviour { this: MasterActor =>
+  implicit val mat = ActorMaterializer()(context.system)
 
   val failed: Receive = {
-    case Stop => self ! Kill
+    case Stop => context stop self
   }
 
   val waitingForResult: Receive = {
+    case Failure(exception) =>
+      context stop sender
+      log.error(exception, "error while executing task")
     case TaskResult(result: Map[(String, String), Int]) =>
-      println(result)
+      val fs = FileSystem.get(new Configuration())
+      Source(result)
+        .map(m => HdfsWriteMessage(ByteString(s"${m._1._1}\t${m._1._2}\t${m._2}")))
+        .via(HdfsFlow.data(
+          fs,
+          SyncStrategy.count(3),
+          RotationStrategy.none,
+          HdfsWritingSettings(
+            overwrite = false,
+            newLine = true,
+            lineSeparator = System.lineSeparator(),
+            pathGenerator = FilePathGenerator(
+              (rotationCount, timestamp) => s"$outDir/akka-yarn-$rotationCount-$timestamp.csv"
+            )
+          )
+      )).runWith(Sink.ignore)
   }
 
   val running: Receive = {
     case RunTasks =>
       cluster.state.members
-        .filter(_.hasRole("tworker"))
-        .map(m => RootActorPath(m.address) / "user" / "tworker")
+        .filter(_.hasRole(WorkerActorRole))
+        .map(m => RootActorPath(m.address) / RootActorName / WorkerActorRole)
         .map(context.system.actorSelection)
         .foreach(_ ! Task(self, filePath))
 
